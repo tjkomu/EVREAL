44040aaf60e4b6eab1355c37633407d999675fd6
Tuomo Komulainen <tuomo.komulainen@kovilta.fi>
2024-02-01 16:14:29 +0200

    optimize bag_to_npy.py memory usage

--- tools/bag_to_npy.py
+++ tools/bag_to_npy.py
@@ -23,30 +23,47 @@ def bag_to_npy(bag_path, output_pth, event_topic, image_topic):
     image_event_indices_path = os.path.join(output_pth, 'image_event_indices.npy')
     sensor_size = None
     topics = [image_topic, event_topic]
-    xs, ys, ts, ps = [], [], [], []
+
+    # events_ts and events_xy_ps are filled using temporary buffers xys_ps_buf and ts_buf
+    xys_ps_buf, ts_buf = [], []
+    events_ts = np.empty(shape=(0,), dtype=np.float32)
+    events_xy_ps = np.empty(shape=(0,3), dtype=np.int16)
+
     image_list, image_ts_list, image_event_indices_list = [], [], []
     bag = rosbag.Bag(bag_path)
-    for topic, msg, t in tqdm(bag.read_messages(topics)):
-        if topic == event_topic:
-            for e in msg.events:
-                timestamp = timestamp_float(e.ts)
-                xs.append(e.x)
-                ys.append(e.y)
-                ts.append(timestamp)
-                ps.append(1 if e.polarity else 0)
-        elif topic == image_topic:
-            timestamp = timestamp_float(msg.header.stamp)
-            image_ts_list.append(timestamp)
-            image = CvBridge().imgmsg_to_cv2(msg, "mono8")
-            image_list.append(image)
-            if sensor_size is None:
-                sensor_size = image.shape[:2]
-    bag.close()
 
-    events_ts = np.array(ts)
-    events_xy = np.array([xs, ys]).transpose()
-    events_p = np.array(ps)
+    print("reading event information")
+    for topic, msg, t in tqdm(bag.read_messages(event_topic)):
+        for e in msg.events:
+            timestamp = timestamp_float(e.ts)
+            xys_ps_buf.append((e.x, e.y, 1 if e.polarity else 0))
+            ts_buf.append(timestamp)
+
+        # for each 1M events, purge buffer to extend the numpy array
+        if len(ts_buf) > 2**20:
+            events_ts = np.concatenate([events_ts, ts_buf])
+            ts_buf.clear()
+
+            events_xy_ps = np.concatenate([events_xy_ps, xys_ps_buf], dtype=np.int16)
+            xys_ps_buf.clear()
+
+    # put remainders to the destination arrays and delete buffers
+    events_ts = np.concatenate([events_ts, ts_buf])
+    del ts_buf
+
+    events_xy_ps = np.concatenate([events_xy_ps, xys_ps_buf], dtype=np.int16)
+    del xys_ps_buf
+
+    print("reading image information")
+    for topic, msg, t in tqdm(bag.read_messages(image_topic)):
+        timestamp = timestamp_float(msg.header.stamp)
+        image_ts_list.append(timestamp)
+        image = CvBridge().imgmsg_to_cv2(msg, "mono8")
+        image_list.append(image)
+        if sensor_size is None:
+            sensor_size = image.shape[:2]
     # assert np.all(events_ts[:-1] <= events_ts[1:])
+    bag.close()
 
     images = np.stack(image_list)
     images = np.expand_dims(images, axis=-1)
@@ -67,8 +84,8 @@ def bag_to_npy(bag_path, output_pth, event_topic, image_topic):
     image_event_indices = np.clip(image_event_indices, 0, len(events_ts) - 1)
 
     np.save(events_ts_path, events_ts, allow_pickle=False, fix_imports=False)
-    np.save(events_xy_path, events_xy, allow_pickle=False, fix_imports=False)
-    np.save(events_p_path, events_p, allow_pickle=False, fix_imports=False)
+    np.save(events_xy_path, events_xy_ps[:,:2], allow_pickle=False, fix_imports=False)
+    np.save(events_p_path, events_xy_ps[:,2], allow_pickle=False, fix_imports=False)
 
     np.save(images_path, images, allow_pickle=False, fix_imports=False)
     np.save(images_ts_path, images_ts, allow_pickle=False, fix_imports=False)
